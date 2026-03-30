ed29c271076f69557c63039263fca9aad998b9b7
3yTpa <gosyanman@gmail.com>
2026-03-30 23:53:37 +0300

    Update modules/screen/__init__.py for sequence

--- modules/screen/__init__.py
+++ modules/screen/__init__.py
@@ -1,16 +1,17 @@
 """
 Модуль экранов приложения Тренажёр памяти.
-Содержит различные экраны: главное меню, игры, настройки и т.д.
 """
 
 from .menu import MenuScreen
 from .login import LoginScreen
 from .settings import SettingsScreen
 from .match_pairs import MatchPairsScreen
+from .sequence import SequenceScreen
 
 __all__ = [
     'MenuScreen',
     'LoginScreen',
     'SettingsScreen',
     'MatchPairsScreen',
+    'SequenceScreen',
 ]
